aa5b88fb9fe2c0a1ceb8c3dde027781391af6018
Raghu Betina <rbetina@gmail.com>
2013-07-17 08:18:48 -0500

    Removed example routes for individual horoscopes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,4 @@ FortuneTeller::Application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
   get("/horoscopes", { :controller => "horoscopes", :action => "index" })
-
-  get("/horoscopes/aries", { :controller => "horoscopes", :action => "aries" })
-  get("/horoscopes/taurus", { :controller => "horoscopes", :action => "taurus" })
-  get("/horoscopes/gemini", { :controller => "horoscopes", :action => "gemini" })
-  get("/horoscopes/cancer", { :controller => "horoscopes", :action => "cancer" })
-  get("/horoscopes/leo", { :controller => "horoscopes", :action => "leo" })
-  get("/horoscopes/virgo", { :controller => "horoscopes", :action => "virgo" })
-  get("/horoscopes/libra", { :controller => "horoscopes", :action => "libra" })
-  get("/horoscopes/scorpio", { :controller => "horoscopes", :action => "scorpio" })
-  get("/horoscopes/sagittarius", { :controller => "horoscopes", :action => "sagittarius" })
-  get("/horoscopes/capricorn", { :controller => "horoscopes", :action => "capricorn" })
-  get("/horoscopes/aquarius", { :controller => "horoscopes", :action => "aquarius" })
-  get("/horoscopes/pisces", { :controller => "horoscopes", :action => "pisces" })
 end
