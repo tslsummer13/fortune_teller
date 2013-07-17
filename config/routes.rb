4c538cbb35f62471f1426002507a6536bc9ea14d
Raghu Betina <rbetina@gmail.com>
2013-07-17 09:12:22 -0500

    Added the last 11 routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,15 @@ FortuneTeller::Application.routes.draw do
 
   # [GET] "/horoscopes/aries"
   get "/horoscopes/aries" => "horoscopes#aries"
+  get "/horoscopes/taurus" => "horoscopes#taurus"
+  get "/horoscopes/gemini" => "horoscopes#gemini"
+  get "/horoscopes/cancer" => "horoscopes#cancer"
+  get "/horoscopes/leo" => "horoscopes#leo"
+  get "/horoscopes/virgo" => "horoscopes#virgo"
+  get "/horoscopes/libra" => "horoscopes#libra"
+  get "/horoscopes/scorpio" => "horoscopes#scorpio"
+  get "/horoscopes/sagittarius" => "horoscopes#sagittarius"
+  get "/horoscopes/capricorn" => "horoscopes#capricorn"
+  get "/horoscopes/aquarius" => "horoscopes#aquarius"
+  get "/horoscopes/pisces" => "horoscopes#pisces"
 end
