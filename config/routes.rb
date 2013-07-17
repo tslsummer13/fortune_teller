eecb3651ae1648f5fbd83a299e14ac6a72de7835
Raghu Betina <rbetina@gmail.com>
2013-07-17 08:56:22 -0500

    Added first individual horoscope route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 FortuneTeller::Application.routes.draw do
   root :to => "fortunes#numbers"
 
-  get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
+  # get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
+  get "/lucky_numbers", :controller => "fortunes", :action => "numbers"
 
-  get("/horoscopes", { :controller => "horoscopes", :action => "index" })
+  # get("/horoscopes", { :controller => "horoscopes", :action => "index" })
+  get "/horoscopes" => "horoscopes#index"
+
+  # [GET] "/horoscopes/aries"
+  get "/horoscopes/aries" => "horoscopes#aries"
 end
